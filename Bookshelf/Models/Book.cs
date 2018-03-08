c66cca6c9453a986cf073188eb9577ef6505116f
RJ Kline <rj.kline@gmail.com>
2018-03-08 15:24:43 -0500

    first commit

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey ("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        //Navigation property, relationship between tables
+    }
+}
